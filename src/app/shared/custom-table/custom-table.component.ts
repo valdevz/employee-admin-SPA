d26894970467afacf238fe11be33a054f4072e7c
jv_h2d <jorge.valdez@h2d.app>
2022-09-28 17:57:55 -0500

    make local search with the Input decorator data

--- src/app/shared/custom-table/custom-table.component.ts
+++ src/app/shared/custom-table/custom-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, AfterContentChecked } from '@angular/core';
 import { ɵBrowserAnimationBuilder } from '@angular/platform-browser/animations';
 import { faPen, faCheck, faTrash, IconDefinition, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { SweetAlertMessageService } from 'src/app/services/shared/sweet-alert-message.service';
@@ -8,7 +8,7 @@ import { SweetAlertMessageService } from 'src/app/services/shared/sweet-alert-me
   templateUrl: './custom-table.component.html',
   styleUrls: ['./custom-table.component.scss']
 })
-export class CustomTableComponent implements OnInit {
+export class CustomTableComponent implements OnInit, AfterContentChecked {
   @Input( 'obj' ) obj: Array<any>;
   @Input( 'str' ) title: string;
   @Output() action = new EventEmitter<any>();
@@ -19,10 +19,33 @@ export class CustomTableComponent implements OnInit {
   editStatus: boolean = false;
   indexT: number | null = null;
   editedField: string = '';
+  showingResults: Array<any> = [];
+  firstLoad: boolean = true;
 
-  constructor( private sweetAlertSvc: SweetAlertMessageService, ) { }
+  constructor( private sweetAlertSvc: SweetAlertMessageService, ) {}
 
   ngOnInit(): void {}
+
+  ngAfterContentChecked(): void {
+    if(this.obj.length > 0 && this.firstLoad ){
+      this.firstLoad = false;
+      this.showingResults = this.obj;
+    }
+  }
+
+  searchItem( name: string ){
+    if (name.length > 0){
+      let matchArr = []
+      let regex = new RegExp(`(${name.normalize( "NFD" ).replace(/[\u0300-\u036f]/g, "")})`, 'gi')
+      matchArr = this.obj.filter( item => {
+        let noAccents = item.name.normalize( "NFD" ).replace(/[\u0300-\u036f]/g, "")
+        if( regex.test(noAccents) ) return item.name;
+      })
+      this.showingResults = matchArr;
+    } else {
+      this.showingResults = this.obj;
+    }
+  }
 
   editFunc( value: string, index: number ){
     this.editStatus = !this.editStatus;
